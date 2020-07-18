db8a48b4b22cefe4867f28be75cc9e7097a2e9b3
Pazuzu92 <tika.92@mail.ru>
2020-07-18 12:14:44 +0300

    complete mainActivity

--- app/src/main/java/com/example/fruitturn/MainActivity.java
+++ app/src/main/java/com/example/fruitturn/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.fruitturn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView btnReader = findViewById(R.id.list_btn);
+        ImageView btnWheel = findViewById(R.id.wheel_btn);
+
+        btnReader.setOnClickListener(this);
+        btnWheel.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.list_btn:
+                Intent intent = new Intent(this, ActivityViewPager.class);
+                startActivity(intent);
+                break;
+            case R.id.wheel_btn:
+                Intent intent1 = new Intent(this, WheelActivity.class);
+                startActivity(intent1);
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/fruitturn/WheelActivity.java
@@ -0,0 +1,14 @@
+package com.example.fruitturn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WheelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wheel);
+    }
+}
